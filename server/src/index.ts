c2775077913ffd7901f0b56387ac708a5a153554
Luke Maloney <lukemaloney@Lukes-MacBook-Pro-2.local>
2024-09-23 11:33:24 -0700

    feat: added test route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import mongo from '@/config/mongo';
+import routes from '@/routes/routes';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+  origin: [`http://localhost:3000`],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 const startServer = async () => {
   try {
     await mongo.connect();
+
+    app.use(routes);
 
     app.listen(3001, () => {
       console.log('> Server running on http://localhost:3001');
