bef98d76816f21756df629ec4b13d4aaaad88028
Eric Galvan <lericgalvan@outlook.com>
2024-11-04 00:28:48 -0800

    Added navigation menu with dummy links.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomeProvider } from '@/context/HomeContext';
 
 import Home from './home/Home';
+import Navigation from './navigation/Navigation';
 
 export default function RootPage() {
   return (
     <HomeProvider>
+      <Navigation />
       <Home />
     </HomeProvider>
   );
